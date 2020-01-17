fed729a36929d12dea1ddee5adc7bfb564fdbd49
Dejan Predojevic <dejanpredojevic11@gmail.com>
2020-01-17 19:49:39 +0100

    registracija zavrsena i logovanje

---
+++ naucnaCentrala/src/main/java/ftn/uns/ac/rs/dto/LoginResponse.java
@@ -0,0 +1,10 @@
+package ftn.uns.ac.rs.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginResponse {
+
+	String username;
+	String role;
+}
--- naucnaCentrala/src/main/resources/data.sql
+++ naucnaCentrala/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into user_data(id, email, grad, ime, odobren_recenzent, password, prezime, recenzent, scientific_area, titula, username) values 
-(1,'dejan@gmail.com','kula','dejan',true,'dejan','dejan',true,'bla bla' , 'ima', 'dejan');
+insert into user_data(id, email, grad, ime, odobren_recenzent, password, prezime, recenzent, scientific_area, titula, username, role) values 
+(1,'demo@gmail.com','kula','demo',true,'demo','demo',true,'demo' , 'demo', 'demo', 'admin');
 insert into scientific_area(id, scientific_area) values (1, 'Naucna oblast 1');
 insert into scientific_area(id, scientific_area) values (2, 'Naucna oblast 2');
 insert into scientific_area(id, scientific_area) values (3, 'Naucna oblast 3');
